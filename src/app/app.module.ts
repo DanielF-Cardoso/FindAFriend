d240a5e28957715d59072461060e15c885e62bdb
DanielF-Cardoso <danielfca122@gmail.com>
2025-01-17 14:09:20 -0300

    chore(env): configure environment variables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,18 @@
 import { OrganizationModule } from '@/modules/orgs/infra/http/organization.module'
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './env/env'
+import { EnvModule } from './env/env.module'
 
 @Module({
-  imports: [OrganizationModule],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    OrganizationModule,
+    EnvModule,
+  ],
   controllers: [],
   providers: [],
 })
