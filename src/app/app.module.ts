1f30d89891d05f4da3c4b01a72918dde8ffdd25a
DanielF-Cardoso <danielfca122@gmail.com>
2025-01-17 22:07:43 -0300

    chore(auth): implement JWT authentication

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env/env'
 import { EnvModule } from './env/env.module'
+import { AuthModule } from './auth/auth.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { EnvModule } from './env/env.module'
     }),
     OrganizationModule,
     EnvModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
--- src/app/cryptography/cryptography.module.ts
+++ src/app/cryptography/cryptography.module.ts
@@ -2,12 +2,15 @@ import { HasherComparer } from '@/modules/orgs/cryptography/hash-comparer'
 import { Module } from '@nestjs/common'
 import { BcryptHasher } from './bcrypt-hasher'
 import { HasherGenerator } from '@/modules/orgs/cryptography/hash-generator'
+import { Encrypter } from '@/modules/orgs/cryptography/encrypter'
+import { JwtEncrypter } from './jwt-encrypter'
 
 @Module({
   providers: [
     { provide: HasherComparer, useClass: BcryptHasher },
     { provide: HasherGenerator, useClass: BcryptHasher },
+    { provide: Encrypter, useClass: JwtEncrypter },
   ],
-  exports: [HasherComparer, HasherGenerator],
+  exports: [HasherComparer, HasherGenerator, Encrypter],
 })
 export class CryptographyModule {}
